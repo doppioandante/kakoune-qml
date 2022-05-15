dad2972467893bf3e7534672fd4a724616b9c22c
Enrico Lumetti <enrico.lumetti@gmail.com>
2022-05-15 22:14:27 +0200

    Add splitwindow executable template

--- src/splitmain.qml
+++ src/splitmain.qml
@@ -1,5 +1,20 @@
-import QtQuick 2.0
+import QtQuick 2.7
+import QtQuick.Window 2.2
 
-Item {
+Window {
+    visible: true
+    title: "Kakoune"
 
+    width: 600
+    height: 400
+
+    SingleWindow {
+      objectName: "kakounePane"; // needed by findChild() in main.cpp
+
+      fontFamily: "Monospace";
+
+      anchors.fill: parent;
+      visible: true;
+      focus: true;
+    }
 }
---
+++ src/splitwindow.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cassert>
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QProcess>
+
+#include "kakoune_client.h"
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine(QUrl("qrc:/splitmain.qml"));
+
+    QObject* kakPane = engine.rootObjects().constFirst()->findChild<QObject*>("kakounePane");
+    assert(kakPane != nullptr);
+
+    KakouneClient clt{"kakouneqml", kakPane};
+    QObject::connect(&clt, SIGNAL(subprocess_finished(int,QProcess::ExitStatus)), &app, SLOT(quit()));
+    return app.exec();
+}
